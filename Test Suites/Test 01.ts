f3b7c404f9e65d8cda88f9c0379543165250c1eb
rifqihusainpratama@outlook.co.id <rifqihusainpratama@outlook.co.id>
2023-12-04 22:19:55 +0700

    Update 22.19 041223

--- Test Suites/Test 01.ts
+++ Test Suites/Test 01.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>1b3eab40-1bf5-4c53-af44-85eb5cc7f9e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>dbc90e41-63c9-41da-b1ce-ce248d5550ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SKENARIO/01.Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
